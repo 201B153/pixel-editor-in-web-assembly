693e57f5a3980fd1adbe0a629e36d3ac0f3e786b
201b153 <tomarm698@gmail.com>
2022-08-30 00:50:42 +0530

    Finishing

--- webassembly-pixel-editor/src/lib.rs
+++ webassembly-pixel-editor/src/lib.rs
@@ -4,16 +4,27 @@
 #![allow(non_snake_case)]
 use std::iter::FromIterator;
 
-use im:Vector;
+use im::Vector;
 use wasm_bindgen::prelude::*;
-use web_sys::console
-#[sfg(feature = "wee_alloc")]
+use web_sys::console;
+
+// When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
+// allocator.
+//
+// If you don't want to use `wee_alloc`, you can safely delete this.
+#[cfg(feature = "wee_alloc")]
 #[global_allocator]
-static ALLOC: wee_alloc:WeeAlloc = weealloc::WeeAlloc::INIT;
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+// This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
+    // This provides better error messages in debug mode.
+    // It's disabled in release mode so it doesn't bloat up the file size.
     #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
+
+    // Your code goes here!
     console::log_1(&JsValue::from_str("Webassembly loaded"));
 
     Ok(())
@@ -21,7 +32,7 @@ pub fn main_js() -> Result<(), JsValue> {
 
 #[wasm_bindgen]
 pub fn addsome(a: i32, b: i32) -> i32 {
-    a + b 
+    a + b
 }
 
 #[wasm_bindgen]
@@ -29,13 +40,15 @@ pub fn mainmock() {
     std::thread::spawn(|| println!("whatever"));
     std::thread::spawn(|| println!("whatever2"));
 }
+
 #[wasm_bindgen]
 #[derive(Clone, PartialEq)]
 struct Rgb {
     r: u8,
     g: u8,
-    b: u8.
+    b: u8,
 }
+
 #[wasm_bindgen]
 #[derive(Clone, PartialEq)]
 pub struct Image {
@@ -46,12 +59,20 @@ pub struct Image {
 
 #[wasm_bindgen]
 impl Image {
+    //#[wasm_bindgen(constructor)]
+    //use like new Image()
     pub fn new(height: usize, width: usize) -> Self {
-        let cells = Vector::from_iteer((0..height * width).map(|i| {
+        // let mut cells = Vec::new();
+        //cells.resize(width * height, Rgb { r: 2, g: 2, b: 2 });
+
+        let cells = Vector::from_iter((0..height * width).map(|i| {
             return Rgb {
                 r: 200,
                 g: 200,
                 b: 255,
+                //r: (455 - i) as u8,
+                //g: (455 - i) as u8,
+                //b: (455 - i) as u8,
             };
         }));
         Image {
@@ -74,6 +95,7 @@ impl Image {
     pub fn getHeight(&self) -> usize {
         self.height
     }
+
     pub fn getWidth(&self) -> usize {
         self.width
     }
@@ -86,9 +108,9 @@ impl Image {
             b: color[2],
         };
     }
-    
-    pub fn brush(&self, x: usize, color: Vec<u8>) -> Self {
-        let index = y * selfwidth + x;
+
+    pub fn brush(&self, x: usize, y: usize, color: Vec<u8>) -> Self {
+        let index = y * self.width + x;
 
         let newCell = self.cells.update(
             index,
@@ -96,7 +118,7 @@ impl Image {
                 r: color[0],
                 g: color[1],
                 b: color[2],
-            };
+            },
         );
 
         Self {
@@ -108,22 +130,36 @@ impl Image {
 }
 
 #[wasm_bindgen]
-pub struct InterState {
+pub struct InternalState {
+    undo_queue: UndoQueue<Image>,
+}
+
+#[wasm_bindgen]
+impl InternalState {
+    //use like new Image()
+    // #[wasm_bindgen(constructor)]
+    // pub fn new(height: usize, width: usize) -> Self {
+
     #[wasm_bindgen(constructor)]
     pub fn new(height: usize, width: usize) -> Self {
         Self {
             undo_queue: UndoQueue::new(Image::new(height, width)),
         }
     }
+
     pub fn getCurrent(&self) -> Image {
         self.undo_queue.getCurrent()
     }
+
     pub fn push(&self) -> Image {
         self.undo_queue.getCurrent()
     }
-    pub fn brush(&mut self, x: usize, y: usize, color: Vec<8>) {
+
+    pub fn brush(&mut self, x: usize, y: usize, color: Vec<u8>) {
         let image = self.undo_queue.getCurrent().clone();
         let topush = image.brush(x, y, color);
+
+        //No need to push the update if canvas looks the same
         if topush != image {
             self.undo_queue.push(topush);
         }
@@ -132,20 +168,26 @@ pub struct InterState {
     pub fn undo(&mut self) {
         self.undo_queue.undo();
     }
+
     pub fn redo(&mut self) {
         self.undo_queue.redo();
     }
+
     pub fn start_dragging(&mut self) {
+        self.undo_queue.setMode(DrawingMode::StartDragging);
+    }
+
+    pub fn stop_dragging(&mut self) {
         self.undo_queue.setMode(DrawingMode::Normal);
     }
 }
 
 enum DrawingMode {
     Normal,
-    start_dragging,
+    StartDragging,
     DraggingProgress,
 }
-Struct UndoQueue<T: Clone> {
+struct UndoQueue<T: Clone> {
     queue: Vec<T>,
     index: usize,
     mode: DrawingMode,
@@ -158,21 +200,23 @@ impl<T: Clone> UndoQueue<T> {
             mode: DrawingMode::Normal,
         }
     }
+
     fn getCurrent(&self) -> T {
         self.queue[self.index].clone()
     }
+
     fn push(&mut self, entry: T) {
         match self.mode {
             DrawingMode::Normal => {
                 self.queue.truncate(self.index + 1);
                 self.queue.push(entry);
-                //to reflect the above push on getCurrent
+                //to reflect the above push on getCurretn
                 self.index += 1;
             }
-            DrawingMode::start_dragging => {
+            DrawingMode::StartDragging => {
                 self.queue.truncate(self.index + 1);
                 self.queue.push(entry);
-                // to refelect the above push on getCurrent
+                //to reflect the above push on getCurretn
                 self.index += 1;
                 self.mode = DrawingMode::DraggingProgress
             }
@@ -181,12 +225,21 @@ impl<T: Clone> UndoQueue<T> {
             }
         }
     }
+
     fn undo(&mut self) {
-        console::log_1(&JsValue::from_str("undoinfo"));
+        console::log_1(&JsValue::from_str("undoing"));
         if self.index > 0 {
             self.index -= 1;
         }
     }
+
+    fn redo(&mut self) {
+        console::log_1(&JsValue::from_str("redoing"));
+        if self.index < self.queue.len() - 1 {
+            self.index += 1;
+        }
+    }
+
     fn setMode(&mut self, mode: DrawingMode) {
         self.mode = mode;
     }
@@ -196,24 +249,3 @@ impl<T: Clone> UndoQueue<T> {
 pub fn mulbyhundred(a: i32) -> i32 {
     a * 100
 }
-
-// When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
-// allocator.
-//
-// If you don't want to use `wee_alloc`, you can safely delete this.
-
-
-// This is like the `main` function, except for JavaScript.
-// #[wasm_bindgen(start)]
-// pub fn main_js() -> Result<(), JsValue> {
-    // This provides better error messages in debug mode.
-    // It's disabled in release mode so it doesn't bloat up the file size.
-//     #[cfg(debug_assertions)]
-//     console_error_panic_hook::set_once();
-
-
-    // Your code goes here!
-//     console::log_1(&JsValue::from_str("Hello world!"));
-
-//     Ok(())
-// }
